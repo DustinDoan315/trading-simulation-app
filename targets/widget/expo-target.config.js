f445c65304ca5839ea53d7a3c69336b67f49eaa1
Dustin Doan <doankhanhdong3105@gmail.com>
2025-12-09 10:32:07 +0700

    update widget

--- targets/widget/expo-target.config.js
+++ targets/widget/expo-target.config.js
@@ -1,18 +1,10 @@
 /** @type {import('@bacons/apple-targets/app.plugin').ConfigFunction} */
-module.exports = (config) => ({
+module.exports = config => ({
   type: "widget",
-  icon: "https://github.com/expo.png",
+  icon: 'https://github.com/expo.png',
   entitlements: {
-    "com.apple.security.application-groups": [
-      "group.com.dustindoan.tradecoinskills",
-    ],
+    "com.apple.security.application-groups": ["group.com.dustin-widget.shared"],
   },
-  //color
-  color: "#000000",
-  backgroundColor: "#000000",
-  borderColor: "#000000",
-  borderWidth: 1,
-  borderRadius: 10,
-  borderStyle: "solid",
-  borderOpacity: 1,
+  frameworks: ["SwiftUI", "ActivityKit"],
+  backgroundColor: "blue",
 });
